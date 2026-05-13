052f6f0c0c66d5aa5f752ba9b808e33f47eedb96
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-05-12 22:02:21 -0300

    Concluido Post destaque, criado um componente reutilizavel para os posts, e deixado grid responsiva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostCoverImage } from '@/components/PostCoverImage';
+import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
-import Link from 'next/link';
-import Image from 'next/image';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
@@ -11,16 +11,30 @@ export default async function HomePage() {
     <Container>
       <Header />
       <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
-          <Image
-            className='group-hover:scale-105 transition'
-            src='/images/bryen_0.png'
-            width={1024}
-            height={768}
-            alt='Título'
-          />
-        </Link>
-        <div></div>
+        <PostCoverImage
+          linkProps={{
+            href: `#`,
+          }}
+          imageProps={{
+            alt: 'Título',
+            width: 1200,
+            height: 720,
+            src: '/images/bryen_8.png',
+            priority: true,
+          }}
+        />
+        <div className='flex flex-col gap-4 sm:justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2026-05-12'
+          >
+            12/05/2026
+          </time>
+          <PostHeading as='h1' url='#'>
+            Título
+          </PostHeading>
+          <p>Post</p>
+        </div>
       </section>
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -1,12 +1,40 @@
 import { postRepository } from '@/repositories/post';
+import { PostCoverImage } from '../PostCoverImage';
+import { PostHeading } from '../PostHeading';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
 
   return (
-    <div>
+    <div className='grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3'>
       {posts.map(post => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div className='flex group flex-col gap-4' key={post.id}>
+            <PostCoverImage
+              linkProps={{
+                href: `/post/${post.slug}`,
+              }}
+              imageProps={{
+                alt: post.title,
+                width: 1200,
+                height: 720,
+                src: post.coverImageUrl,
+              }}
+            />
+            <div className='flex flex-col gap-4 sm:justify-center'>
+              <time
+                className='text-slate-600 block text-sm/tight'
+                dateTime={post.createdAt}
+              >
+                {post.createdAt}
+              </time>
+              <PostHeading as='h2' url='#'>
+                {post.title}
+              </PostHeading>
+              <p>{post.excerpt}</p>
+            </div>
+          </div>
+        );
       })}
     </div>
   );
