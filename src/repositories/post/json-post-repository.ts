9c8049655d292ec3f8316e2cd427a49d5b017207
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-05-20 22:05:00 -0300

    Estilizado os single post, criado componente para a função de formatação de datetime, utilizado markdown para renderizar HTML no conteúdo do post, e utilizado plugin tailwindcss/typography para finalizações

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -12,7 +12,7 @@ const JSON_POSTS_FIEL_PATH = resolve(
   'posts.json',
 );
 
-const SIMULATE_WAIT_IN_MS = 2000;
+const SIMULATE_WAIT_IN_MS = 0;
 
 export class JsonPostRepository implements PostRepository {
   private async simulateWait() {
