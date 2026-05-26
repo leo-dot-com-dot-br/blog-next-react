ea44525863911f9ba63fd6c0e5a0f17bb5000238
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-05-25 22:03:42 -0300

    Adicionado elemento do HTML com Markdown, criado componente 'use client' para erros e criado rota para captura de erros inesperados

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,11 @@
-import clsx from 'clsx';
+import ErrorMessage from '@/components/ErrorMessage';
 
 export default function NotFoundPage() {
   return (
-    <>
-      <title>Página não encontrada!</title>
-      <div
-        className={clsx(
-          'min-h-80 bg-slate-900 text-slate-100',
-          'mb-16 p-8 rounded-xl',
-          'flex items-center justify-center',
-          'text-center',
-        )}
-      >
-        <div>
-          <h1 className='text-7xl/tight mb-4 font-extrabold'> ERRO 404</h1>
-          <p>Erro 404 - Página inexistente.</p>
-        </div>
-      </div>
-    </>
+    <ErrorMessage
+      pageTitle='Página não encontrada!'
+      contentTitle='404'
+      content='Erro 404 - Página inexistente.'
+    />
   );
 }
--- src/components/SafeMarkdown/index.tsx
+++ src/components/SafeMarkdown/index.tsx
@@ -26,6 +26,16 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return '';
+            return (
+              <div className='overflow-x-auto'>
+                <table className='w-full min-w-150' {...props} />
+              </div>
+            );
+          },
+        }}
       >
         {markdown}
       </ReactMarkdown>
