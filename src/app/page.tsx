2026e8b0ad2dd43a9b7fd28be7a86ce32dc1d186
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-06-02 21:45:35 -0300

    Buildado projeto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <>
-      <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
-        <PostFeatured />
-        <PostsList />
-      </Suspense>
-    </>
+    <Suspense fallback={<SpinLoader className='min-h-20 mb-16' />}>
+      <PostFeatured />
+      <PostsList />
+    </Suspense>
   );
 }
