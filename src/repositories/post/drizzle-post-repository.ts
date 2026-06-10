d0771ffda20a349ec5d79f053f8bf5a26b6e75a4
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-06-09 21:58:13 -0300

    Criado DrizzlePostRepository para a base de dados do projeto

---
+++ src/repositories/post/drizzle-post-repository.ts
@@ -0,0 +1,42 @@
+import { PostModel } from '@/models/post/post-model';
+import { PostRepository } from './post-repository';
+import { drizzleDb } from '@/db/drizzle';
+
+export class DrizzlePostRepository implements PostRepository {
+  async findAllPublic(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+      where: (posts, { eq }) => eq(posts.published, true),
+    });
+    return posts;
+  }
+
+  async findBySlugPublic(slug: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq, and }) =>
+        and(eq(posts.published, true), eq(posts.slug, slug)),
+    });
+    if (!post) throw new Error('Slug de post não encontrado!');
+    return post;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    const posts = await drizzleDb.query.posts.findMany({
+      orderBy: (posts, { desc }) => desc(posts.createdAt),
+    });
+    return posts;
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const post = await drizzleDb.query.posts.findFirst({
+      where: (posts, { eq }) => eq(posts.id, id),
+    });
+    if (!post) throw new Error('ID de post não encontrado!');
+    return post;
+  }
+}
+
+// (async () => {
+//   const repo = new DrizzlePostRepository();
+//   await repo.findAllPublic;
+// })();
