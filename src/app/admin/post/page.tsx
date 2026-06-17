59a25b01f5c91a8240eb792c108bbe57a15d530c
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-06-16 21:36:22 -0300

    Usado unstable_cache para cache nas funções, criado rotas administrativas e separado queries em public e admin

---
+++ src/app/admin/post/page.tsx
@@ -0,0 +1,20 @@
+import { findAllPostAdmin } from '@/lib/post/queries/admin';
+import { Metadata } from 'next';
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'Post Admin',
+};
+
+export default async function AdminPostPage() {
+  const posts = await findAllPostAdmin();
+
+  return (
+    <div className='py-16'>
+      {posts.map(post => {
+        return <p key={post.id}>{post.title}</p>;
+      })}
+    </div>
+  );
+}
