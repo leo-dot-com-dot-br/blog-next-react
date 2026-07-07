8da828c1072bb6428015968f320337d6c83a16a2
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-07-06 21:49:00 -0300

    Usado typescript para adicionar variações de estilos em componentes como botão e input

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,11 +1,12 @@
-import { Button } from '@/components/Button';
+import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-dynamic';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 text-6xl'>
-      <Button></Button>
+    <div className='flex flex-col gap-6'>
+      <InputText labelText='' placeholder='Digite seu nome' />
+      <InputText labelText='' placeholder='Digite seu sobrenome' />
     </div>
   );
 }
