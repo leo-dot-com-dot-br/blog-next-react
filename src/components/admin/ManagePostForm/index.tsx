3d7b3a15b72a2181039c32fc2941349806db352c
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-07-13 21:37:59 -0300

    criado markdown com react-md-editor e imageuploader para upload de imagens

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -3,14 +3,27 @@
 import { Button } from '@/components/Button';
 import { InputCheckbox } from '@/components/InputCheckbox';
 import { InputText } from '@/components/InputText';
+import { MarkdownEditor } from '@/components/MarkdownEditor';
+import { useState } from 'react';
+import { ImageUploader } from '../ImageUploader';
 
 export function ManagePostForm() {
+  const [contentValue, setContentValue] = useState('');
+
   return (
     <form action='' className='mb-16'>
       <div className='flex flex-col gap-6'>
         <InputText labelText='' placeholder='Digite seu nome' />
+        <ImageUploader />
         <InputText labelText='' placeholder='Digite seu sobrenome' />
         <InputCheckbox />
+        <MarkdownEditor
+          labelText='Conteúdo'
+          disabled={false}
+          textAreaName='content'
+          value={contentValue}
+          setValue={setContentValue}
+        />
       </div>
       <div className='mt-4'>
         <Button type='submit'>Enviar</Button>
