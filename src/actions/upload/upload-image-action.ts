b54a7cde47794ef11bafd2bc52f2579b33fdc4e9
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-07-14 21:57:54 -0300

    validado tamanho do arquivo no front e no backend e validado tipo de arquivo que será enviado

---
+++ src/actions/upload/upload-image-action.ts
@@ -0,0 +1,34 @@
+'use server';
+
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
+
+type UploadImageActionResult = {
+  url: string;
+  error: string;
+};
+
+export async function uploadImageAction(
+  formData: FormData,
+): Promise<UploadImageActionResult> {
+  const makeResult = ({ url = '', error = '' }) => ({ url, error });
+
+  if (!(formData instanceof FormData)) {
+    return makeResult({ error: 'Dados inválidos' });
+  }
+
+  const file = formData.get('file');
+
+  if (!(file instanceof File)) {
+    return makeResult({ error: 'Arquivo inválido' });
+  }
+
+  if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+    return makeResult({ error: 'Arquivo muito grande' });
+  }
+
+  if (!file.type.startsWith('image/')) {
+    return makeResult({ error: 'Imagem inválida' });
+  }
+
+  return makeResult({ url: 'URL' });
+}
--- src/components/admin/ImageUploader/index.tsx
+++ src/components/admin/ImageUploader/index.tsx
@@ -1,16 +1,58 @@
 'use client';
 
+import { uploadImageAction } from '@/actions/upload/upload-image-action';
 import { Button } from '@/components/Button';
+import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
 import { ImageUpIcon } from 'lucide-react';
-import { useRef } from 'react';
+import { useRef, useTransition } from 'react';
+import { toast } from 'react-toastify';
 
 export function ImageUploader() {
   const fileInputRef = useRef<HTMLInputElement>(null);
+  const [isUploading, startTransition] = useTransition();
 
   function handleChooseFile() {
     if (!fileInputRef.current) return;
 
     fileInputRef.current.click();
+  }
+
+  function handleChange() {
+    toast.dismiss();
+
+    if (!fileInputRef.current) return;
+
+    const fileInput = fileInputRef.current;
+    const file = fileInput?.files?.[0];
+
+    if (!file) return;
+
+    if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+      const readableMaxSize = IMAGE_UPLOAD_MAX_SIZE / 1024;
+      toast.error(
+        `Tamanho de imagem não suportado. Máx.: ${readableMaxSize}KB`,
+      );
+
+      fileInput.value = '';
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    startTransition(async () => {
+      const result = await uploadImageAction(formData);
+
+      if (result.error) {
+        toast.error(result.error);
+        fileInput.value = '';
+        return;
+      }
+
+      toast.success(result.url);
+    });
+
+    fileInput.value = '';
   }
 
   return (
@@ -26,6 +68,7 @@ export function ImageUploader() {
         name='file'
         type='file'
         accept='image/*'
+        onChange={handleChange}
       />
     </div>
   );
