54b61e5a698dc6d297dfa97d3c31a5804868a7ed
Leonardo de Brito <leonardode.b.c@gmail.com>
2026-07-28 16:32:52 -0300

    Adicionado componente LoginForm e actions de login e logout. Adicionado lib bcrypt para crição de hash de senha, criado função para criar hash de senha. Adicionado cookies para sessão de login e tempo de expirar

---
+++ src/actions/login/login-action.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import { createLoginSession, verifyPassword } from '@/lib/login/manage-login';
+import { asyncDelay } from '@/utils/async-delay';
+import { redirect } from 'next/navigation';
+
+type loginActionState = {
+  username: string;
+  error: string;
+};
+
+export async function loginAction(state: loginActionState, formData: FormData) {
+  await asyncDelay(2000);
+
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Dados inválidos',
+    };
+  }
+
+  const username = formData.get('username')?.toString().trim() || '';
+  const password = formData.get('password')?.toString().trim() || '';
+
+  if (!username || !password) {
+    return {
+      username,
+      error: 'Digite o usuário e a senha',
+    };
+  }
+
+  const isUsernameValid = username === process.env.LOGIN_USER;
+  const isPasswordValid = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || '',
+  );
+
+  if (!isUsernameValid || !isPasswordValid) {
+    return {
+      username,
+      error: 'Usuário ou senha inválidos',
+    };
+  }
+
+  await createLoginSession(username);
+  redirect('/admin/post');
+}
